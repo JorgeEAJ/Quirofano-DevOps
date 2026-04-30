9ec1e1038745168beb1ef16a509d266c24cdb6c2
unknown <arizmendi.jorge.cbtis56@gmail.com>
2026-04-29 19:52:45 -0600

    Paciente sano: Linter y Prettier aplicados

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 # 1. Error de calidad: Variable declarada que NO se usa (Flake8 lo marcará)
 medicina_vencida = "Paracetamol 500mg"
 
+
 def clasificar_paciente(gravedad):
-# 2. Error de formato: Espaciado caótico e indentación desastrosa (Black lo arreglará)
-    nombre =  input("Ingrese nombre del paciente: ")
-    mensaje= ""
 
-# 3. Error de lógica: Comparación poco clara (Flake8 sugerirá mejores prácticas)
-    if gravedad == 'critico':
-# 4. Error de formato: Mezcla de comillas simples y dobles, y falta de espacios
-      mensaje ="🚨 Alerta Roja: Quirófano inmediato para "  +nombre
-    elif gravedad=='estable':
-      mensaje = '✅ Paciente ' + nombre + ' asignado a sala de espera'
+    nombre = input("Ingrese nombre del paciente: ")
+    mensaje = ""
 
-# 5. Error de formato: Espacios extra antes de los paréntesis
-    print ( mensaje )
+    # 3. Error de lógica: Comparación poco clara
+    if gravedad == "critico":
+
+        mensaje = "🚨 Alerta Roja: Quirófano inmediato para " + nombre
+    elif gravedad == "estable":
+        mensaje = "✅ Paciente " + nombre + " asignado a sala de espera"
+
+    # 5. Error de formato: Espacios extra antes de los paréntesis
+    print(mensaje)
+
 
 # 6. Función fantasma que nadie llamó (Otro error de Flake8)
 def hospital_cerrado():
     return True
+
 
 # 7. Error de estilo: Código pegado al final sin línea en blanco (PEP 8)
 print("Sistema de triaje iniciado")
